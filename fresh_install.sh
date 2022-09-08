a4fb0a6047a1e373002058073e1b93f2657ad4ac
Sérgio Pereira <xsergiop@gmail.com>
2022-09-08 17:55:40 +0100

    Install neovim

--- fresh_install.sh
+++ fresh_install.sh
@@ -31,3 +31,11 @@ stow lazygit
 
 # FZF
 nala install fzf -y
+
+# Neovim
+nala install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen -y
+git clone https://github.com/neovim/neovim.git
+cd neovim
+git checkout release-0.7
+make CMAKE_BUILD_TYPE=Release
+make install
