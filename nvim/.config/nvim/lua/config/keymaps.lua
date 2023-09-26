8c6a3e5751efb6b54e838161b9ae85fca1a09f25
sergio.pereira <sergio.pereira@rtcom.pt>
2023-09-26 17:11:52 +0100

    added move line keymaps for macos Option key

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move Lines
+vim.keymap.set("n", "¯", "<cmd>m .+1<cr>==", { desc = "Move down" })
+vim.keymap.set("n", "„", "<cmd>m .-2<cr>==", { desc = "Move up" })
+vim.keymap.set("i", "¯", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+vim.keymap.set("i", "„", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+vim.keymap.set("v", "¯", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "„", ":m '<-2<cr>gv=gv", { desc = "Move up" })
